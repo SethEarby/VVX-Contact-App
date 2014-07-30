6ccf442d34f75177c87e591527daefb00a8253f0
Seth Earby <fenderfan23@gmail.com>
2014-07-29 23:40:18 -0500

    Adding base project files for v.1

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 VVX-Contact-App
 ===============
-
+#v.1beta
 Generates XML contact data for the Polycom VVX series phones.  
+
+This was created to remove some of the monotony out of using XML Notepad 2007. (Recommended by the documentation to use)
+No multiple select, will not edit file if there is a syntax error, and a bunch of erroneous behavior can be caused by shortcuts.
+
+*Created using Angular.js and the Twitter Bootstrap Frameworks*
---
+++ app.js
@@ -0,0 +1,26 @@
+
+var app = angular.module('MyTutorialApp',[]);
+
+app.controller("MainController", function($scope){
+    $scope.num = 3;
+    $scope.users = [];
+
+    $scope.addContact = function()
+    {
+
+        $scope.users.push({
+            Ext: $scope.Ext,
+            name: $scope.name,
+            num: $scope.num,
+            context: $scope.context
+        });
+        $scope.num++;
+        $scope.Ext ="";
+        $scope.name="";
+        $scope.context="";
+        console.log($scope.num);
+        console.log($scope.name);
+    }
+});
+
+$
